30b910c216ffad15910717c8edb0b9a3649eda98
Nikhil Thapa <tnikhil.official@gmail.com>
2023-07-29 00:33:25 +0545

    version 1.0.0

--- index.js
+++ index.js
@@ -1,36 +1,30 @@
-const ViberBot = require('viber-bot').Bot,
-  BotEvents = require('viber-bot').Events,
-  TextMessage = require('viber-bot').Message.Text,
-  express = require('express');
-const app = express();
+const ngrok = require('./get_public_url');
+const ViberBot = require('viber-bot').Bot;
+const BotEvents = require('viber-bot').Events;
 
-if (!process.env.BOT_ACCOUNT_TOKEN) {
-  console.log('Could not find bot account token key.');
-  return;
-}
-if (!process.env.EXPOSE_URL) {
-  console.log('Could not find exposing url');
-  return;
-}
-
+// Creating the bot with access token, name and avatar
 const bot = new ViberBot({
-  authToken: process.env.BOT_ACCOUNT_TOKEN,
-  name: "Cat Vet Bot",
-  avatar: "https://upload.wikimedia.org/wikipedia/commons/3/3d/Katze_weiss.png"
+  authToken: '51671da7e267e410-1f5893c6d15df4f-b68b6c86e947b4eb',
+  name: 'myechobot',
+  avatar: 'https://3de6-27-34-108-199.ngrok.io',
 });
-bot.on(BotEvents.SUBSCRIBED, response => {
-  response.send(new TextMessage(`Hi there ${response.userProfile.name}. I am ${bot.name}! Feel free to ask me anything.`));
-});
+
 bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
-  response.send(new TextMessage(`Message received.`));
+  // Echo's back the message to the client.
+  response.send(message);
 });
-const port = process.env.PORT || 3000;
-app.use("/viber/webhook", bot.middleware());
-app.listen(port, () => {
-  console.log(`Application running on port: ${port}`);
-  bot.setWebhook(`${process.env.EXPOSE_URL}/viber/webhook`).catch(error => {
-    console.log('Can not set webhook on following server. Is it running?');
+
+const http = require('http');
+const port = process.env.PORT || 8080;
+return ngrok
+  .getPublicUrl()
+  .then((publicUrl) => {
+    console.log('Set the new webhook to"', publicUrl);
+    http
+      .createServer(bot.middleware())
+      .listen(port, () => bot.setWebhook(publicUrl));
+  })
+  .catch((error) => {
+    console.log('Can not connect to ngrok server. Is it running?');
     console.error(error);
-    process.exit(1);
   });
-});
